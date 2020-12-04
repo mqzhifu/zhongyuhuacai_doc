fab4ef22a14b7cfa69404584bd1cfaf33806fbda
mqzhifu <mqzhifu@sina.com>
2020-12-04 17:41:29 +0800

    dd

--- import_mysql.sh
+++ import_mysql.sh
@@ -13,7 +13,7 @@ DIR=`pwd`
 #chmod 777 -R $STORAGE_DIR
 #chmod 777 -R $UPLOAD_DIR
 
-
+mysql_cmd=/soft/mysql/bin/mysql
 
 
 export MYSQL_PWD=${PS}
@@ -23,11 +23,11 @@ echo "$HOST:$PORT $DB $USER $PS $DIR";
 DEL_DB="drop database ${DB}"
 ADD_DB="create database ${DB} charset=utf8mb4"
 
-mysql  -h${HOST} -u${USER} --default-character-set=utf8  -e"${DEL_DB};${ADD_DB};"
+$mysql_cmd  -h${HOST} -u${USER} --default-character-set=utf8  -e"${DEL_DB};${ADD_DB};"
 echo ${DEL_DB}
 echo ${ADD_DB}
 
-mysql  -h${HOST} -u${USER} --default-character-set=utf8 -D${DB}  -e "source ${DIR}/structure.sql"
+$mysql_cmd  -h${HOST} -u${USER} --default-character-set=utf8 -D${DB}  -e "source ${DIR}/structure.sql"
 echo "source ${DIR}/structure.sql"
 
 
@@ -35,7 +35,7 @@ echo "source ${DIR}/structure.sql"
 for file in ./init_data/*
 do
 echo $file
-mysql  -h${HOST} -u${USER} --default-character-set=utf8 -D${DB} -e"source ${DIR}/${file}"
+$mysql_cmd  -h${HOST} -u${USER} --default-character-set=utf8 -D${DB} -e"source ${DIR}/${file}"
 done
 
 
